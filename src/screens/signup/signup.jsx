bf24bab510405baef70160ce35e8aa77e38c8793
razatta <rizwan@devsurge.co>
2022-11-29 11:47:07 +0500

    firebase basic attach with signup

--- src/screens/signup/signup.jsx
+++ src/screens/signup/signup.jsx
@@ -5,9 +5,13 @@ import { Header } from "../../components/header";
 import { Input } from "../../components/input";
 import { TextButton } from "../../components/textButton";
 import { colors, modifiers } from "../../utils/theme";
+import { firebase } from "../../services/firebaseConfig";
 
 function Signup() {
   const [showPass, setShowPass] = useState(false);
+  const [userName, setUserName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
 
   const handleShowPass = () => {
     if (showPass === true) {
@@ -15,6 +19,15 @@ function Signup() {
     } else if (showPass === false) {
       setShowPass(true);
     }
+  };
+
+  const onSignupPress = () => {
+    console.log(userName, email, password);
+    firebase.firestore().collection("users").doc("id0002").set({
+      user_name: userName,
+      user_email: email,
+      user_password: password,
+    });
   };
 
   return (
@@ -26,18 +39,21 @@ function Signup() {
         <Input
           placeholder={"User Name"}
           showIcon={true}
+          onChange={setUserName}
           iconName={"person-outline"}
         />
 
         <Input
           placeholder={"Email"}
           showIcon={true}
+          onChange={setEmail}
           iconName={"mail-outline"}
         />
 
         <Input
           placeholder={"Password"}
           isSecure={!showPass}
+          onChange={setPassword}
           showIcon={true}
           iconName={showPass === false ? "eye-outline" : "eye-off-outline"}
           onIconPress={handleShowPass}
@@ -46,7 +62,7 @@ function Signup() {
         <View style={styles.textBtnCon}>
           <TextButton title={"Already have an account?"} />
         </View>
-        <BButton title={"Sign up"} />
+        <BButton title={"Sign up"} onButtonPress={onSignupPress} />
       </View>
     </ScrollView>
   );
---
+++ src/services/firebaseConfig.js
@@ -0,0 +1,19 @@
+import * as firebase from "firebase";
+import "@firebase/firestore";
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDemnK2tN58msfDZ_anW2tmbFgrjcrXUwA",
+  authDomain: "cookito-backend.firebaseapp.com",
+  projectId: "cookito-backend",
+  storageBucket: "cookito-backend.appspot.com",
+  messagingSenderId: "952427219556",
+  appId: "1:952427219556:web:b8d3ba4dab7e8978adad71",
+  measurementId: "G-BJ8PL35NR6",
+};
+// if user has already opened app atleasonce connect with FIREBASE
+if (firebase.apps.length > 0 === false) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export { firebase };
